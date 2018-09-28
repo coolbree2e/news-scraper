0fd103f1e4adfdaa928727be3ef2b307fdbb5dcb
Colby Schumm <coolbreeze@Colbys-MacBook-Air.local>
2018-09-28 10:02:03 -0600

    I HATE THIS

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ app.set("view engine", "handlebars");
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.medium.com/topic/science/").then(function(response) {
+  axios.get("http://www.theonion.com").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     const $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("section h3,a").each(function(i, element) {
+    // Now, we grab every h1, h3, h4, h6  within an article tag, and do the following:
+    $("h1,h6").each(function(i, element) {
       // Save an empty result object
       const result = {};
 
@@ -49,12 +49,10 @@ app.get("/scrape", function(req, res) {
       result.title = $(this)
         .children("a")
         .text();
-      result.link = $(this)
+      result.link =  $(this)
         .children("a")
         .attr("href");
-        result.summary = $(this)
-        .children("a")
-        .text();
+       
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
